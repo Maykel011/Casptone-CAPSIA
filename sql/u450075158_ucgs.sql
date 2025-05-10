b5a9b4b2d835bf9fef0dd487dc967f76de5fadf0
Maykel011 <montes.johnmichael01120@gmail.com>
2025-05-10 14:08:46 +0800

    commit

--- sql/u450075158_ucgs.sql
+++ sql/u450075158_ucgs.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 04, 2025 at 05:45 PM
+-- Generation Time: May 10, 2025 at 08:07 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -68,7 +68,7 @@ CREATE TABLE `borrow_requests` (
   `return_date` date NOT NULL,
   `purpose` text NOT NULL,
   `notes` text DEFAULT NULL,
-  `status` enum('Pending','Approved','Rejected','Returned','For Checking','Processed','Not Accepted') NOT NULL,
+  `status` enum('Pending','Approved','Rejected','For Checking','For Releasing','Released','Return Pending','Returned') NOT NULL DEFAULT 'Pending',
   `request_date` datetime NOT NULL DEFAULT current_timestamp(),
   `transaction_id` int(11) DEFAULT NULL,
   `item_name` varchar(255) DEFAULT NULL,
@@ -77,6 +77,14 @@ CREATE TABLE `borrow_requests` (
   `return_status` varchar(20) DEFAULT NULL,
   `return_reason` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `borrow_requests`
+--
+
+INSERT INTO `borrow_requests` (`borrow_id`, `user_id`, `item_id`, `quantity`, `date_needed`, `return_date`, `purpose`, `notes`, `status`, `request_date`, `transaction_id`, `item_name`, `processed_at`, `rejection_reason`, `return_status`, `return_reason`) VALUES
+(1, 19, 21, 1, '2025-05-10', '2025-05-10', 'QWE', 'QWE', 'Returned', '2025-05-10 10:29:58', NULL, 'Microphone', '2025-05-10 10:34:29', NULL, NULL, NULL),
+(2, 19, 13, 1, '2025-05-10', '2025-05-10', 'QWE', 'QWE', 'Released', '2025-05-10 10:44:47', NULL, 'Plastic Table', '2025-05-10 10:45:08', NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -112,26 +120,26 @@ INSERT INTO `items` (`item_id`, `created_by`, `item_no`, `item_name`, `descripti
 (1, 1, 'ITEM-67fa9921bc165', 'ESV Bible', 'ESV Bible', 200, 200, 'pcs', 'Available', 'ITEM-257280217', 'stationary', 'Admin Office', NULL, NULL, NULL, '2025-05-04 18:02:56', '2025-04-12 16:47:29'),
 (2, 1, 'ITEM-67fa995d32e2e', 'KJV Bible', 'KJV Bible', 100, 100, 'pcs', 'Available', 'ITEM-377690816', 'stationary', 'Storage Room', NULL, NULL, NULL, '2025-05-04 18:02:07', '2025-04-12 16:48:29'),
 (3, 1, 'ITEM-67fa99aa78f13', 'NIV Bible', 'NIV Bible', 150, 150, 'pcs', 'Available', 'ITEM-302507392', 'stationary', 'Admin Office', NULL, NULL, NULL, '2025-05-04 18:02:07', '2025-04-12 16:49:46'),
-(4, 1, 'ITEM-67fa9a0349d71', 'Projector', 'Projector', 5, 5, 'pcs', 'Available', 'ITEM-751601536', 'electronics', 'Storage Room', NULL, NULL, NULL, '2025-05-04 18:02:07', '2025-04-12 16:51:15'),
+(4, 1, 'ITEM-67fa9a0349d71', 'Projector', 'Projector', 5, 4, 'pcs', 'Available', 'ITEM-751601536', 'electronics', 'Storage Room', NULL, NULL, NULL, '2025-05-09 16:04:10', '2025-04-12 16:51:15'),
 (5, 1, 'ITEM-67fa9ae1c1337', 'Brown Envelope', 'Brown Envelope', 1000, 1000, 'pcs', 'Available', 'ITEM-102478715', 'stationary', 'Admin Office', NULL, NULL, NULL, '2025-05-04 18:02:07', '2025-04-12 16:54:57'),
-(6, 1, 'ITEM-67fa9b10312a0', 'Cardboard', 'Cardboard', 124, 124, 'pcs', 'Available', 'ITEM-972015773', 'consumables', 'Storage Room', NULL, NULL, NULL, '2025-05-04 18:02:07', '2025-04-12 16:55:44'),
-(7, 1, 'ITEM-67fa9b60744bc', 'First Aid Kit', 'First Aid Kit', 20, 16, 'pcs', 'Available', 'ITEM-477741107', 'consumables', 'Admin Office', NULL, NULL, NULL, '2025-05-04 23:14:03', '2025-04-12 16:57:04'),
+(6, 1, 'ITEM-67fa9b10312a0', 'Cardboard', 'Cardboard', 119, 120, 'pcs', 'Available', 'ITEM-972015773', 'consumables', 'Storage Room', NULL, NULL, NULL, '2025-05-09 16:35:53', '2025-04-12 16:55:44'),
+(7, 1, 'ITEM-67fa9b60744bc', 'First Aid Kit', 'First Aid Kit', 47, 45, 'pcs', 'Available', 'ITEM-477741107', 'consumables', 'Admin Office', NULL, NULL, NULL, '2025-05-10 10:27:21', '2025-04-12 16:57:04'),
 (8, 1, 'ITEM-67fa9c5f18095', 'A4 Bond Paper', 'A4 Bond Paper', 10, 10, 'bdl', 'Available', 'ITEM-324759139', 'stationary', 'Admin Office', NULL, NULL, NULL, '2025-05-04 18:02:07', '2025-04-12 17:01:19'),
 (9, 1, 'ITEM-67fa9c8f1f5be', 'Long Bond Paper', 'Long Bond Paper', 11, 11, 'bdl', 'Available', 'ITEM-932141824', 'stationary', 'Admin Office', NULL, NULL, NULL, '2025-05-04 18:02:07', '2025-04-12 17:02:07'),
-(10, 1, 'ITEM-67fa9d6f220b6', 'Office Chair', 'Office Chair', 10, 10, 'pcs', 'Available', 'ITEM-166943995', 'furniture', 'Admin Office', NULL, NULL, NULL, '2025-05-04 18:02:07', '2025-04-12 17:05:51'),
-(11, 1, 'ITEM-67fa9e6da1407', 'Laptop', 'Galaxy Book5 Pro 360', 6, 6, 'pcs', 'Available', 'ITEM-476850205', 'electronics', 'Admin Office', NULL, NULL, NULL, '2025-05-04 18:02:07', '2025-04-12 17:10:05'),
-(12, 1, 'ITEM-67fa9f0361d7a', 'Mini Trash Bin', 'Mini Trash Bin', 4, 4, 'pcs', 'Available', 'ITEM-381597685', 'accessories', 'Admin Office', NULL, NULL, NULL, '2025-05-04 18:02:07', '2025-04-12 17:12:35'),
-(13, 1, 'ITEM-67faa018c3a67', 'Plastic Table', 'Plastic Table', 12, 12, 'pcs', 'Available', 'ITEM-972015773', 'furniture', 'Storage Room', NULL, NULL, NULL, '2025-05-04 18:02:07', '2025-04-12 17:17:12'),
+(10, 1, 'ITEM-67fa9d6f220b6', 'Office Chair', 'Office Chair', 10, 10, 'pcs', 'Available', 'ITEM-166943995', 'furniture', 'Admin Office', NULL, NULL, NULL, '2025-05-10 09:06:12', '2025-04-12 17:05:51'),
+(11, 1, 'ITEM-67fa9e6da1407', 'Laptop', 'Galaxy Book5 Pro 360', 7, 9, 'pcs', 'Available', 'ITEM-476850205', 'electronics', 'Admin Office', NULL, NULL, NULL, '2025-05-10 09:06:13', '2025-04-12 17:10:05'),
+(12, 1, 'ITEM-67fa9f0361d7a', 'Mini Trash Bin', 'Mini Trash Bin', 1, 3, 'pcs', 'Available', 'ITEM-381597685', 'accessories', 'Admin Office', NULL, NULL, NULL, '2025-05-09 17:07:15', '2025-04-12 17:12:35'),
+(13, 1, 'ITEM-67faa018c3a67', 'Plastic Table', 'Plastic Table', 11, 11, 'pcs', 'Available', 'ITEM-972015773', 'furniture', 'Storage Room', NULL, NULL, NULL, '2025-05-10 10:44:50', '2025-04-12 17:17:12'),
 (14, 1, 'ITEM-67faa0b58d5b7', 'Crayons', 'Crayola', 10, 10, 'bx', 'Available', 'ITEM-106819365', 'stationary', 'Admin Office', NULL, NULL, NULL, '2025-05-04 18:02:07', '2025-04-12 17:19:49'),
 (15, 1, 'ITEM-67faa0fb20d1f', 'White Envelope', 'White Envelope', 500, 500, 'pcs', 'Available', 'ITEM-644439902', 'stationary', 'Admin Office', NULL, NULL, NULL, '2025-05-04 18:02:07', '2025-04-12 17:20:59'),
 (16, 1, 'ITEM-67faa154f0150', 'Broom', 'Broom', 10, 10, 'pcs', 'Available', 'ITEM-519293501', 'stationary', 'Janitorial Office', NULL, NULL, NULL, '2025-05-04 18:02:07', '2025-04-12 17:22:28'),
-(17, 1, 'ITEM-67faa2ea64404', 'Disposable cups', 'Disposable cups', 5, 5, 'bx', 'Available', 'ITEM-381597685', 'consumables', 'Admin Office', NULL, NULL, NULL, '2025-05-04 18:02:07', '2025-04-12 17:29:14'),
-(18, 1, 'ITEM-67faa3ce3c8e1', 'Communion Wine', 'Communion Wine', 14, 14, 'pcs', 'Available', 'ITEM-922704471', 'consumables', 'Admin Office', NULL, NULL, NULL, '2025-05-04 18:02:07', '2025-04-12 17:33:02'),
-(19, 1, 'ITEM-67faa3f12194b', 'Candles', 'White Candles', 4, 4, 'bdl', 'Low Stock', 'ITEM-328217189', 'consumables', 'Admin Office', NULL, NULL, NULL, '2025-05-04 18:02:07', '2025-04-12 17:33:37'),
+(17, 1, 'ITEM-67faa2ea64404', 'Disposable cups', 'Disposable cups', 2, 5, 'bx', 'Available', 'ITEM-381597685', 'consumables', 'Admin Office', NULL, NULL, NULL, '2025-05-10 10:16:21', '2025-04-12 17:29:14'),
+(18, 1, 'ITEM-67faa3ce3c8e1', 'Communion Wine', 'Communion Wine', 12, 14, 'pcs', 'Available', 'ITEM-922704471', 'consumables', 'Admin Office', NULL, NULL, NULL, '2025-05-09 17:19:02', '2025-04-12 17:33:02'),
+(19, 1, 'ITEM-67faa3f12194b', 'Candles', 'White Candles', 3, 4, 'bdl', 'Low Stock', 'ITEM-328217189', 'consumables', 'Admin Office', NULL, NULL, NULL, '2025-05-06 01:16:18', '2025-04-12 17:33:37'),
 (20, 1, 'ITEM-67faa548bf415', 'Paper Plates', 'Paper Plates', 234, 234, 'pcs', 'Available', 'ITEM-166943995', 'consumables', 'Storage Room', NULL, NULL, NULL, '2025-05-04 18:02:07', '2025-04-12 17:39:20'),
-(21, 1, 'ITEM-67faa59c68191', 'Microphone', 'Microphone', 18, 18, 'pcs', 'Available', 'ITEM-163744751', 'electronics', 'Sanctuary', NULL, NULL, NULL, '2025-05-04 18:02:07', '2025-04-12 17:40:44'),
-(22, 1, 'ITEM-67faa6160c955', 'Music Stand', 'Black Music Stand', 8, 8, 'pcs', 'Available', 'ITEM-552270458', 'furniture', 'Sanctuary', NULL, NULL, NULL, '2025-05-04 18:02:07', '2025-04-12 17:42:46'),
-(23, 1, 'ITEM-67faa677a23e4', 'Camera', 'Canon EOS 3000D with 18-55 III Non-IS Black', 6, 6, 'pcs', 'Available', 'ITEM-328217189', 'electronics', 'Sanctuary', NULL, NULL, NULL, '2025-05-04 18:02:07', '2025-04-12 17:44:23'),
+(21, 1, 'ITEM-67faa59c68191', 'Microphone', 'Microphone', 16, 17, 'pcs', 'Available', 'ITEM-163744751', 'electronics', 'Sanctuary', NULL, NULL, NULL, '2025-05-10 10:34:29', '2025-04-12 17:40:44'),
+(22, 1, 'ITEM-67faa6160c955', 'Music Stand', 'Black Music Stand', 6, 6, 'pcs', 'Available', 'ITEM-552270458', 'furniture', 'Sanctuary', NULL, NULL, NULL, '2025-05-09 16:04:13', '2025-04-12 17:42:46'),
+(23, 1, 'ITEM-67faa677a23e4', 'Camera', 'Canon EOS 3000D with 18-55 III Non-IS Black', 5, 5, 'pcs', 'Available', 'ITEM-328217189', 'electronics', 'Sanctuary', NULL, NULL, NULL, '2025-05-06 01:05:45', '2025-04-12 17:44:23'),
 (24, 1, 'ITEM-67faa6d92e780', 'Flatscreen TV', 'Daewoo Electronic Appliances LED Google TV 32 Inches', 6, 6, 'pcs', 'Available', 'ITEM-106819365', 'electronics', 'Sanctuary', NULL, NULL, NULL, '2025-05-04 18:02:07', '2025-04-12 17:46:01'),
 (25, 1, 'ITEM-67faa7d447e99', 'Erasers', 'Erasers', 35, 35, 'pcs', 'Available', 'ITEM-106819365', 'stationary', 'Admin Office', NULL, NULL, NULL, '2025-05-04 18:02:07', '2025-04-12 17:50:12'),
 (26, 1, 'ITEM-67fbaa271459f', 'Wooden Table', 'Wooden Table', 8, 8, 'pcs', 'Available', 'ITEM-654719365', 'furniture', 'Sanctuary', NULL, NULL, NULL, '2025-05-04 18:02:07', '2025-04-13 12:12:23'),
@@ -169,6 +177,13 @@ CREATE TABLE `item_returns` (
   `created_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `item_returns`
+--
+
+INSERT INTO `item_returns` (`return_id`, `user_id`, `item_id`, `category`, `quantity`, `return_date`, `item_condition`, `notes`, `status`, `created_at`) VALUES
+(1, 19, 21, '0', 1, '2025-05-10', 'Good', '', '', '2025-05-10 10:34:23');
+
 -- --------------------------------------------------------
 
 --
@@ -189,6 +204,13 @@ CREATE TABLE `new_item_requests` (
   `ministry` enum('UCM','CWA','CHOIR','PWT','CYF') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `new_item_requests`
+--
+
+INSERT INTO `new_item_requests` (`request_id`, `user_id`, `item_name`, `item_category`, `quantity`, `item_unit`, `purpose`, `notes`, `status`, `request_date`, `ministry`) VALUES
+(1, 19, 'Bible', 'accesories', 5, 'Piece', 'qwe', 'qwe', 'Rejected', '2025-05-10 11:06:02', 'CHOIR');
+
 -- --------------------------------------------------------
 
 --
@@ -203,6 +225,30 @@ CREATE TABLE `notifications` (
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `type` enum('Info','Warning','Error') DEFAULT 'Info'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notificationscart`
+--
+
+CREATE TABLE `notificationscart` (
+  `notification_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `type` enum('approval','rejection','cart_update') NOT NULL,
+  `is_read` tinyint(1) DEFAULT 0,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `notificationscart`
+--
+
+INSERT INTO `notificationscart` (`notification_id`, `user_id`, `message`, `type`, `is_read`, `created_at`) VALUES
+(1, 19, 'Your request for 1 Microphone has been approved and is now For Checking', '', 0, '2025-05-10 02:30:01'),
+(2, 19, 'Your request for 1 Microphone is now For Releasing', 'approval', 0, '2025-05-10 02:30:26'),
+(3, 19, 'Your request for 1 Microphone has been Released', 'approval', 0, '2025-05-10 02:30:28');
 
 -- --------------------------------------------------------
 
@@ -297,6 +343,15 @@ CREATE TABLE `transactions` (
   `request_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `transactions`
+--
+
+INSERT INTO `transactions` (`transaction_id`, `user_id`, `action`, `details`, `created_at`, `item_name`, `quantity`, `status`, `item_id`, `request_id`) VALUES
+(1, 19, 'Borrow', 'Borrowed 1 of item \'Microphone\' in electronics category (from cart)', '2025-05-10 10:29:58', 'Microphone', 1, 'Pending', 21, NULL),
+(2, 19, 'Borrow', 'Borrowed 1 of item \'Plastic Table\' in furniture category (from cart)', '2025-05-10 10:44:47', 'Plastic Table', 1, 'Pending', 13, NULL),
+(3, 19, 'New Item Request', 'Requested 5 Piece of \'Bible\' (accesories)', '2025-05-10 11:06:02', 'Bible', 5, 'Pending', NULL, NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -326,6 +381,24 @@ INSERT INTO `users` (`user_id`, `username`, `email`, `password`, `role`, `minist
 (1, 'Jay Neri Anunuevo', 'jnanunuevo@gmail.com', '$2y$10$JbOjtTvo3X9j3vKe1Gux1O48EUWeLSQhXksz6YWjewqG.UXiD3t.S', 'Administrator', 'CHOIR', 'Active', '2025-04-12 05:28:52', '2025-04-12 15:41:50', NULL, NULL, NULL),
 (19, 'Susan Gasilao', 'susangasilao@yahoo.com', '$2y$10$50P9Nj/mNHsXhpWYkR/v.eppKJE7NdEmKnpjv2HMIUsV9uhmird4y', 'User', 'CHOIR', 'Active', '2025-04-12 17:50:39', '2025-04-13 13:02:36', NULL, NULL, NULL),
 (21, 'Nerizza Joy Mabazza', 'jamaicamabazza0809@gmail.com', '$2y$10$dq/ruk3KWEf/LQIoHFbi5exO85oQXXjkEevKNfhc4ZOL7PKJTDxmC', 'User', 'CYF', 'Deactivated', '2025-04-13 11:25:09', '2025-04-13 13:10:47', '2025-04-14 13:10:47', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_carts`
+--
+
+CREATE TABLE `user_carts` (
+  `user_id` int(11) NOT NULL,
+  `cart_data` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_carts`
+--
+
+INSERT INTO `user_carts` (`user_id`, `cart_data`) VALUES
+(19, '[]');
 
 -- --------------------------------------------------------
 
@@ -401,6 +474,13 @@ ALTER TABLE `notifications`
   ADD KEY `idx_notifications_type` (`type`);
 
 --
+-- Indexes for table `notificationscart`
+--
+ALTER TABLE `notificationscart`
+  ADD PRIMARY KEY (`notification_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
 -- Indexes for table `reports`
 --
 ALTER TABLE `reports`
@@ -448,6 +528,12 @@ ALTER TABLE `users`
   ADD UNIQUE KEY `email` (`email`);
 
 --
+-- Indexes for table `user_carts`
+--
+ALTER TABLE `user_carts`
+  ADD PRIMARY KEY (`user_id`);
+
+--
 -- Indexes for table `user_requests`
 --
 ALTER TABLE `user_requests`
@@ -469,7 +555,7 @@ ALTER TABLE `audit_logs`
 -- AUTO_INCREMENT for table `borrow_requests`
 --
 ALTER TABLE `borrow_requests`
-  MODIFY `borrow_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `borrow_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `items`
@@ -481,19 +567,25 @@ ALTER TABLE `items`
 -- AUTO_INCREMENT for table `item_returns`
 --
 ALTER TABLE `item_returns`
-  MODIFY `return_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `return_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `new_item_requests`
 --
 ALTER TABLE `new_item_requests`
-  MODIFY `request_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `request_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `notifications`
 --
 ALTER TABLE `notifications`
   MODIFY `notification_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `notificationscart`
+--
+ALTER TABLE `notificationscart`
+  MODIFY `notification_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `reports`
@@ -523,7 +615,7 @@ ALTER TABLE `return_requests`
 -- AUTO_INCREMENT for table `transactions`
 --
 ALTER TABLE `transactions`
-  MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -574,6 +666,12 @@ ALTER TABLE `notifications`
   ADD CONSTRAINT `fk_notifications_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 
 --
+-- Constraints for table `notificationscart`
+--
+ALTER TABLE `notificationscart`
+  ADD CONSTRAINT `notificationscart_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
 -- Constraints for table `reports`
 --
 ALTER TABLE `reports`
@@ -605,6 +703,12 @@ ALTER TABLE `transactions`
   ADD CONSTRAINT `fk_transactions_item_id` FOREIGN KEY (`item_id`) REFERENCES `items` (`item_id`),
   ADD CONSTRAINT `fk_transactions_request_id` FOREIGN KEY (`request_id`) REFERENCES `new_item_requests` (`request_id`),
   ADD CONSTRAINT `fk_transactions_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `user_carts`
+--
+ALTER TABLE `user_carts`
+  ADD CONSTRAINT `user_carts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `user_requests`
